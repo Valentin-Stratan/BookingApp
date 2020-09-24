61af69933d318757b0d6b265e26a0059f45ec6ff
Valentin Stratan <intern.valentin.stratan@assist.ro>
2020-09-24 12:13:47 +0300

    admin view bookings by adminId + viewAll bookings of all admins

---
+++ backend/booking/adminBookingView.js
@@ -0,0 +1,63 @@
+'use strict';
+const AWS = require('aws-sdk');
+const db = new AWS.DynamoDB.DocumentClient();
+const uuid = require('uuid');
+const utils = require('../utils/utils');
+const bUtils = require('../utils/booking-utils');
+require('dotenv').config();
+
+async function adminBookingView(event, context, callback) {
+    try {
+        const adminId = event.arguments.adminId;
+
+        // validate provided id
+        const admin = await db.get({
+            TableName: process.env.ADMINS_TABLE,
+            Key: {
+                id: adminId
+            }
+        }).promise();
+
+        if (!admin.Item)
+            return callback(utils.newError('Admin with provided ID not found!!!'), null);
+
+        // getting all the companies of provided admin
+        const companies = await db.query({
+            TableName: process.env.COMPANY_TABLE,
+            IndexName: "adminId-index",
+            KeyConditionExpression: "adminId = :a",
+            ExpressionAttributeValues: {
+                ":a": adminId
+            }
+        }).promise();
+
+
+        // variable for storing all companies of specific admin
+        const bookingViewList = [];
+
+        for (let i = 0; i < companies.Items.length; i++) {
+            const response = await db.query({
+                TableName: process.env.SERVICE_TABLE,
+                IndexName: "companyId-index",
+                KeyConditionExpression: "companyId = :a",
+                ExpressionAttributeValues: {
+                    ":a": companies.Items[i].id
+                }
+            }).promise();
+            // variable for storing company and services
+            const bookingView = {};
+            bookingView.company = companies.Items[i];
+            bookingView.services = response.Items;
+            // storing companies data
+            bookingViewList.push(bookingView);
+        }
+        // if there are no companies or services empty array will be returned
+        return callback(null, bookingViewList);
+
+    }
+    catch (error) {
+        return callback(error, null);
+    }
+}
+
+module.exports.adminBookingView = adminBookingView;
--- backend/booking/booking-handler.js
+++ backend/booking/booking-handler.js
@@ -1,10 +1,20 @@
 'use strict';
 const create = require('./publicBookingCreate');
+const view = require('./adminBookingView');
+const viewAll = require('./adminBookingViewAll');
 
 module.exports.bookingController = async (event, context, callback) => {
     switch (event.field) {
         case 'publicBookingCreate': {
             await create.publicBookingCreate(event, context, callback);
+            break;
+        }
+        case 'adminBookingView': {
+            await view.adminBookingView(event, context, callback);
+            break;
+        }
+        case 'adminBookingViewAll': {
+            await viewAll.adminBookingViewAll(event, context, callback);
             break;
         }
         default: {
