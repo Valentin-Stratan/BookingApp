b2c5485b8f7b2e317c3eef7b5f4d2f46c29392f5
Valentin Stratan <intern.valentin.stratan@assist.ro>
2020-10-02 10:07:53 +0300

    view all reviews of an employee

--- backend/review/review-handler.js
+++ backend/review/review-handler.js
@@ -1,10 +1,15 @@
 'use strict';
 const create = require('./publicReviewCreate');
+const view = require('./publicReviewView');
 
 module.exports.reviewController = async (event, context, callback) => {
     switch (event.field) {
         case 'publicReviewCreate': {
             await create.publicReviewCreate(event, context, callback);
+            break;
+        }
+        case 'publicReviewView': {
+            await view.publicReviewView(event, context, callback);
             break;
         }
         default: {
